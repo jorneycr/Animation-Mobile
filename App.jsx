2fd1dda747dd738a468096ad2a745ec3d525a665
jlopez <jorney95@live.com>
2023-12-15 12:10:16 -0600

    updated son parameters

--- App.jsx
+++ App.jsx
@@ -10,16 +10,16 @@ import {
 // import Animacion1 from './components/Animacion1';
 // import Animacion2 from './components/Animacion2';
 // import Animacion3 from './components/Animacion3';
-import Animacion4 from './components/Animacion4';
+// import Animacion4 from './components/Animacion4';
 // import Animacion5 from './components/Animacion5';
-// import Animacion6 from './components/Animacion6';
+import Animacion6 from './components/Animacion6';
 // import Animacion7 from './components/Animacion7';
 
 const App = () => {
   return (
     <>
       <View style={styles.contenido}>
-        <Animacion4 />
+        <Animacion6 />
       </View>
     </>
   );
